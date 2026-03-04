35a982be4bac872729b720def0a5ec71718479ce
jesmunozr <Jesus_Munoz@epam.com>
2026-03-04 15:25:14 -0500

    Add stories for Counter, Genre and Search components

--- src/app/Search/Search.tsx
+++ src/app/Search/Search.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 
-function Search({ initialQuery, onSearch }: { initialQuery: string, onSearch: (query: string) => void }){
+export interface SearchProps {
+    /** The initial search query to populate the input field with. */
+    initialQuery: string;
+    /** A function to be called when the user submits a search query. */
+    onSearch: (query: string) => void;
+}
+
+/** A search component that allows users to input a movie title and submit a search query. */
+function Search({ initialQuery, onSearch }: SearchProps){
     const [query, setQuery] = React.useState(initialQuery);
     return (
         <div>
@@ -18,9 +26,7 @@ function Search({ initialQuery, onSearch }: { initialQuery: string, onSearch: (q
                     setQuery(event.target.value);
                 }}
             />
-            <button onClick={() => {
-                onSearch(query);
-            }}>Search</button>
+            <button onClick={() => onSearch(query)}>Search</button>
         </div>
     );
 }
